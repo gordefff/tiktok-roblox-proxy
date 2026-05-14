7e0d087ddbfb2cfa1c8fe0225719ce7dfa601a2d
gordefff <gordefb@gmail.com>
2026-05-14 11:43:01 +0700

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 // STATE
 // ============================================
 
-let TIKTOK_USERNAME = "febrydpx";
+let TIKTOK_USERNAME = "dewii.fashionn";
 let chatMessages = [];
 let giftQueue = [];
 const MAX_MESSAGES = 100;
